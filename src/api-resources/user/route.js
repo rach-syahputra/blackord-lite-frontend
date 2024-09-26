0096dce15dc726c2d3c8dd8295b1e3adff149bcf
rach-syahputra <rach.syahputra@gmail.com>
2024-09-26 17:19:26 +0800

    add get current user api resource

--- src/api-resources/user/route.js
+++ src/api-resources/user/route.js
@@ -1,6 +1,9 @@
 import { API_BASE_URL } from '../config'
 
 const USER_ROUTE = `${API_BASE_URL}/users`
-const LOGIN_ROUTE = `${API_BASE_URL}/auth/login`
 
-export { USER_ROUTE, LOGIN_ROUTE }
+const AUTH_ROUTE = `${API_BASE_URL}/auth`
+const LOGIN_ROUTE = `${AUTH_ROUTE}/login`
+const GET_CURRENT_ROUTE = `${AUTH_ROUTE}/current-user`
+
+export { USER_ROUTE, LOGIN_ROUTE, GET_CURRENT_ROUTE }
--- src/api-resources/user/service.js
+++ src/api-resources/user/service.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { LOGIN_ROUTE, USER_ROUTE } from './route'
+import { GET_CURRENT_ROUTE, LOGIN_ROUTE, USER_ROUTE } from './route'
 import Cookies from 'universal-cookie'
 
 const userService = {
@@ -28,9 +28,62 @@ const userService = {
       if (response.status === 200) {
         const accessToken = response.data.data.accessToken
         const cookies = new Cookies()
-        cookies.set('blackordAccessToken', accessToken)
+        cookies.set('blackord-access-token', accessToken)
 
         return { success: true }
+      }
+    } catch (error) {
+      console.error(error.response.data.error)
+
+      if (error.response.status === 500)
+        return { success: false, message: 'something went wrong' }
+
+      return {
+        success: false,
+        error: error.response.data.error
+      }
+    }
+  },
+
+  async logout() {
+    try {
+      const cookies = new Cookies()
+      cookies.remove('blackord-access-token')
+
+      const token = cookies.get('blackord-access-token')
+      if (token) {
+        return {
+          success: false,
+          error: 'logout failed'
+        }
+      } else {
+        return {
+          success: true
+        }
+      }
+    } catch (error) {
+      return {
+        success: false,
+        error: error
+      }
+    }
+  },
+
+  async getCurrentUser() {
+    try {
+      const cookies = new Cookies()
+      const token = cookies.get('blackord-access-token')
+      const response = await axios.get(GET_CURRENT_ROUTE, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      if (response.status === 200) {
+        return {
+          success: true,
+          data: response.data.data
+        }
       }
     } catch (error) {
       console.error(error.response.data.error)
