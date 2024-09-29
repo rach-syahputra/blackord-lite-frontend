9c120bae35914f16cded6b176f6b2c3637efcd36
rach-syahputra <rach.syahputra@gmail.com>
2024-09-29 16:00:56 +0800

    add Get Song by Album Id api

--- src/api-resources/song/service.js
+++ src/api-resources/song/service.js
@@ -4,7 +4,6 @@ import { SONG_ROUTE } from './route'
 
 const songService = {
   async add(request) {
-    console.log('request', request)
     try {
       const cookies = new Cookies()
       const token = cookies.get('blackord-access-token')
@@ -16,6 +15,24 @@ const songService = {
       })
 
       if (response.status === 201) return { success: true }
+    } catch (error) {
+      console.error(error.response.data.error)
+
+      if (response.status === 500) {
+        return {
+          success: false,
+          error: 'something went wrong, please try again'
+        }
+      }
+    }
+  },
+
+  async get(albumId) {
+    try {
+      const response = await axios.get(`${SONG_ROUTE}/albums/${albumId}`)
+
+      if (response.status === 200)
+        return { success: true, data: response.data.data }
     } catch (error) {
       console.error(error.response.data.error)
 
