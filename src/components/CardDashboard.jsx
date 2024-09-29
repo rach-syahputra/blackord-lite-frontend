3be46386fead3abd0d4c161845c576fe83ccd295
rach-syahputra <rach.syahputra@gmail.com>
2024-09-29 16:00:00 +0800

    adjust styles of CardDashboard component

--- src/components/CardDashboard.jsx
+++ src/components/CardDashboard.jsx
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types'
 
 const CardDashboard = ({ name, total }) => {
   return (
-    <div className='flex h-full flex-col items-center justify-center gap-4 overflow-hidden rounded-md border-2 border-black p-4'>
-      <h1 className='text-xl font-bold'>Total {name}</h1>
-      <p className='font-medium'>{total}</p>
+    <div className='flex h-full flex-col items-center justify-center gap-2 overflow-hidden rounded-md border-2 border-black p-4'>
+      <h1 className=''>Total {name}</h1>
+      <p className='text-xl font-bold'>{total}</p>
     </div>
   )
 }
