9da3989467b889bf4bfa7ca1ec30e2989d9ebd12
rach-syahputra <rach.syahputra@gmail.com>
2024-09-29 11:12:59 +0800

    add value props to Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types'
 
-const Input = ({ type, name, placeholder, onChange }) => {
+const Input = ({ type, name, placeholder, value, onChange }) => {
   return (
     <input
       type={type}
       name={name}
       placeholder={placeholder}
       onChange={onChange}
+      value={value}
       className='box-border w-full rounded-md border border-gray-500 p-2 focus:outline-gray-600'
     />
   )
@@ -16,6 +17,7 @@ Input.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string,
   onChange: PropTypes.func
 }
 
