c0980a4aecf9bf234426dd3049ff99c0c5e63495
rach-syahputra <rach.syahputra@gmail.com>
2024-09-29 14:58:52 +0800

    implement protected listener route

--- src/view/listener/Layout.jsx
+++ src/view/listener/Layout.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import ListenerNavbar from '../../components/ListenerNavbar'
 
 const ListenerLayout = () => {
+  const currentUser = useSelector((state) => state.currentUser.data)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!currentUser || currentUser.artistName) navigate('/auth/login')
+  }, [])
+
   return (
     <>
       <ListenerNavbar />
