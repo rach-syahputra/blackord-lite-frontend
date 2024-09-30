f7c80c9680767cd330eeb429603b6eb2b0ee73d3
rach-syahputra <rach.syahputra@gmail.com>
2024-09-30 16:43:05 +0800

    add Get Album API

--- src/api-resources/album/service.js
+++ src/api-resources/album/service.js
@@ -52,6 +52,24 @@ const albumService = {
     }
   },
 
+  async get(id) {
+    try {
+      const response = await axios.get(`${ALBUM_ROUTE}/${id}`)
+
+      if (response.status === 200)
+        return { success: true, data: response.data.data }
+    } catch (error) {
+      console.error(error.response.data.error)
+
+      if (error.response.status === 500) {
+        return {
+          success: false,
+          error: 'something went wrong, please try again'
+        }
+      }
+    }
+  },
+
   async getAllFromArtist(username) {
     try {
       const response = await axios.get(`${ALBUM_ROUTE}/artists/${username}`)
