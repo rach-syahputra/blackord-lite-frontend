3865f28568a545bac2772693d8ea7c98cdf9a7bd
rach-syahputra <rach.syahputra@gmail.com>
2024-09-30 18:04:22 +0800

    display songs list of an album on Album Detail Page

--- src/view/listener/album/detail/Page.jsx
+++ src/view/listener/album/detail/Page.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import albumService from '../../../../api-resources/album/service'
+import songService from '../../../../api-resources/song/service'
 import DetailLoading from '../../../../components/DetailLoading'
+import Song from '../../../../components/Song'
 
 const AlbumDetailPage = () => {
   const params = useParams()
   const { id } = params
   const [album, setAlbum] = useState(null)
+  const [songs, setSongs] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
@@ -15,11 +18,25 @@ const AlbumDetailPage = () => {
 
       if (response.success) {
         setAlbum(response.data)
-        setIsLoading(false)
+        return response.data
       }
     }
 
-    getAlbum()
+    const getSongs = async () => {
+      const response = await songService.getFromAlbum(id)
+
+      if (response.success) {
+        setSongs(response.data)
+        return response.data
+      }
+    }
+
+    const getAlbumAndSongs = async () => {
+      await Promise.all([await getAlbum(), await getSongs()])
+      setIsLoading(false)
+    }
+
+    getAlbumAndSongs()
   }, [])
 
   return (
@@ -33,10 +50,26 @@ const AlbumDetailPage = () => {
             alt='album image'
             className='h-full w-full rounded-md md:max-h-[450px] md:max-w-[450px]'
           />
-          <div className='flex flex-col gap-4'>
-            <h1 className='text-xl font-bold'>{album?.title}</h1>
-            <p>by {album?.artistUsername}</p>
-            <p>{album?.genre}</p>
+          <div className='flex w-full flex-col gap-8'>
+            <section className='flex w-full flex-col gap-4'>
+              <h1 className='text-xl font-bold'>{album?.title}</h1>
+              <p>by {album?.artistUsername}</p>
+              <p>{album?.genre}</p>
+            </section>
+            <section className='flex flex-col gap-4'>
+              <h3 className='font-bold'>Songs</h3>
+              <ul className='flex flex-col gap-2'>
+                {songs?.map((song, index) => (
+                  <li className='flex items-center gap-4' key={song.id}>
+                    <Song
+                      no={index + 1}
+                      title={song.title}
+                      duration={song.duration}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </section>
           </div>
         </>
       )}
