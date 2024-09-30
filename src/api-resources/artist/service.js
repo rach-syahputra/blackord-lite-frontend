6e1af8eb3426d8824bcfbe527b7da292bc3a343f
rach-syahputra <rach.syahputra@gmail.com>
2024-09-30 16:34:24 +0800

    add Get Artist API

--- src/api-resources/artist/service.js
+++ src/api-resources/artist/service.js
@@ -21,6 +21,24 @@ const artistService = {
     }
   },
 
+  async get(username) {
+    try {
+      const response = await axios.get(`${ARTIST_ROUTE}/${username}`)
+
+      if (response.status === 200)
+        return { success: true, data: response.data.data }
+    } catch (error) {
+      console.error(error.response.data.error)
+
+      if (response.status === 500) {
+        return {
+          success: false,
+          error: 'something went wrong, please try again'
+        }
+      }
+    }
+  },
+
   async getAll() {
     try {
       const response = await axios.get(ARTIST_ROUTE)
