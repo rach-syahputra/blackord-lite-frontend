c1025a9f41e1a3b38e5c6e671d88e9a144bedfa9
rach-syahputra <rach.syahputra@gmail.com>
2024-09-30 17:03:08 +0800

    add CardLoading component

---
+++ src/components/CardLoading.jsx
@@ -0,0 +1,13 @@
+const CardLoading = () => {
+  return (
+    <div>
+      <div className='mb-4 h-[240px] w-full rounded-md bg-gray-300 md:h-[280px]' />
+      <div className='flex flex-col gap-2'>
+        <div className='h-4 w-full rounded-md bg-gray-300' />
+        <div className='h-8 w-full rounded-md bg-gray-300' />
+      </div>
+    </div>
+  )
+}
+
+export default CardLoading
