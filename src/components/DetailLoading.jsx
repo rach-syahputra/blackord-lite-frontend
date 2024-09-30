f89a68fc20c448ba1396c678192fc5068b2a25a6
rach-syahputra <rach.syahputra@gmail.com>
2024-09-30 17:11:29 +0800

    add DetailLoading component

---
+++ src/components/DetailLoading.jsx
@@ -0,0 +1,15 @@
+const DetailLoading = () => {
+  return (
+    <>
+      <div className='h-[400px] w-full rounded-md bg-gray-300 md:h-[450px] md:max-w-[450px]' />
+      <div className='flex h-full w-full flex-col gap-4'>
+        <div className='h-8 w-full rounded-md bg-gray-300' />
+        <div className='h-4 w-full rounded-md bg-gray-300' />
+        <div className='h-4 w-full rounded-md bg-gray-300' />
+        <div className='h-4 w-full rounded-md bg-gray-300' />
+      </div>
+    </>
+  )
+}
+
+export default DetailLoading
