50da845d20a7119b9087b7efe843f08055b4ead5
rach-syahputra <rach.syahputra@gmail.com>
2024-09-30 16:35:00 +0800

    add CardArtist component

--- src/components/CardArtist.jsx
+++ src/components/CardArtist.jsx
@@ -1,6 +1,7 @@
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-const CardArtist = ({ image, name, bio }) => {
+const CardArtist = ({ username, image, name, bio }) => {
   return (
     <>
       <img
@@ -9,7 +10,9 @@ const CardArtist = ({ image, name, bio }) => {
         className='h-[240px] w-full rounded-md object-cover object-center md:h-[280px]'
       />
       <div className='flex flex-col gap-2 p-2'>
-        <p className='font-bold'>{name}</p>
+        <Link to={`/artist/${username}`} className='font-bold hover:underline'>
+          {name}
+        </Link>
         <p className='line-clamp-3 text-sm'>{bio}</p>
       </div>
     </>
@@ -17,6 +20,7 @@ const CardArtist = ({ image, name, bio }) => {
 }
 
 CardArtist.propTypes = {
+  username: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   bio: PropTypes.string.isRequired
