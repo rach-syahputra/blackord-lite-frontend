a6aeac1fe313e4170e2b5fe50826a46cc9db6bf2
rach-syahputra <rach.syahputra@gmail.com>
2024-10-01 12:55:20 +0800

    display total followers on Artist Dashboard Page

--- src/view/artist/Page.jsx
+++ src/view/artist/Page.jsx
@@ -3,20 +3,22 @@ import { useSelector } from 'react-redux'
 import albumService from '../../api-resources/album/service'
 import songService from '../../api-resources/song/service'
 import CardDashboard from '../../components/CardDashboard'
+import artistService from '../../api-resources/artist/service'
 
 const ArtistHomePage = () => {
-  const [totalAlbum, setTotalAlbum] = useState(0)
-  const [totalSong, setTotalSong] = useState(0)
+  const [total, setTotal] = useState({
+    albums: 0,
+    songs: 0,
+    followers: 0
+  })
   const currentUser = useSelector((state) => state.currentUser.data)
 
   useEffect(() => {
-    const getTotalAlbumAndSong = async () => {
+    const getTotalAlbumsAndSongs = async () => {
       const response = await albumService.getAllFromArtist(currentUser.username)
 
       if (response.success) {
         const albums = response.data
-
-        setTotalAlbum(albums.length)
 
         const songs = await Promise.all(
           await albums.map(async (album) => {
@@ -28,18 +30,38 @@ const ArtistHomePage = () => {
 
         const totalSong = songs.reduce((acc, val) => acc + val, 0)
 
-        setTotalSong(totalSong)
+        setTotal((prev) => ({
+          ...prev,
+          albums: albums.length,
+          songs: totalSong
+        }))
       }
     }
 
-    getTotalAlbumAndSong()
+    const getTotalFollowers = async () => {
+      const response = await artistService.getFollowers(currentUser.username)
+
+      if (response.success) {
+        setTotal((prev) => ({
+          ...prev,
+          followers: response.data.length
+        }))
+      }
+    }
+
+    const populateArtistDashboardData = async () => {
+      await Promise.all([getTotalAlbumsAndSongs(), getTotalFollowers()])
+    }
+
+    populateArtistDashboardData()
   }, [])
 
   return (
     <div className='py-4 pb-8'>
       <section className='grid grid-cols-2 gap-4 py-4 md:grid-cols-3 lg:grid-cols-4'>
-        <CardDashboard name='Album' total={totalAlbum} />
-        <CardDashboard name='Song' total={totalSong} />
+        <CardDashboard name='Albums' total={total.albums} />
+        <CardDashboard name='Songs' total={total.songs} />
+        <CardDashboard name='Followers' total={total.followers} />
       </section>
     </div>
   )
